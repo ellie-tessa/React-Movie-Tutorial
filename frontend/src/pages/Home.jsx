77e9d43a86585a03aa7a465e6f359b6352f36022
ellie-tessa <elli.tessa@outlook.com>
2026-07-03 17:11:12 +0300

    Searchbar implementation and Favorites page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
   const movies = [
     { id: 1, title: "Parasite", release_date: "2019" },
     { id: 2, title: "The Shawshank Redemption", release_date: "1994" },
     { id: 3, title: "Bugonia", release_date: "2025" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
@@ -15,6 +21,8 @@ function Home() {
           type="text"
           placeholder="Search for a movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
       </form>
 
